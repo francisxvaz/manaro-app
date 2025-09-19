9a380bb8909bfafe2365d23447dfb96cbb19d085
Francis Vaz <francisxvaz@gmail.com>
2025-09-19 10:55:57 +1000

    introduced toaster

--- src/app/components/ProductClientList.tsx
+++ src/app/components/ProductClientList.tsx
@@ -4,7 +4,7 @@ import ProductClientUnit from './ProductClientUnit';
 import { useCart } from './CartContext';
 import { IProduct } from '../models/Product';
 import { Recycle, Download } from "lucide-react";
-
+import { toast } from "sonner";
 
 interface ProductClientListProps {
     products: IProduct[];
@@ -25,41 +25,61 @@ export default function ProductClientList({ products }: ProductClientListProps) 
     }
 
     function deleteLocalStorage(): void {
-        clearCart()
+        toast.error('Your are about to clear the storage', {
+            action: {
+                label: 'Clear',
+                onClick: () => clearCart()
+            },
+        })
+
     }
 
     async function exportCSV(eanArray: string[]) {
-        if(eanArray.length <= 0)
-        {
-            console.log('empty')
+        if (eanArray.length <= 0) {
+            toast.error("No Products!", {
+                description: `No items to export.`,
+            });
             return;
         }
-        const response = await fetch("/api/exportProducts", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ eanArray }),
-        });
 
-        if (response.ok) {
-            const blob = await response.blob();
-            const url = window.URL.createObjectURL(blob);
-            const link = document.createElement("a");
-            link.href = url;
-            link.setAttribute("download", "products.csv");
-            document.body.appendChild(link);
-            link.click();
-            link.remove();
-            //JSON.stringify('RESPONSE' + response)
-        } else {
-            console.error("CSV export failed");
-        }
+        toast.success(`You sure you want to export ${cartItems.length} to csv?`, {
+            action: {
+                label: 'Export',
+                onClick: async () => {
+                    const response = await fetch("/api/exportProducts", {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({ eanArray }),
+                    });
+
+                    if (response.ok) {
+                        const blob = await response.blob();
+                        const url = window.URL.createObjectURL(blob);
+                        const link = document.createElement("a");
+                        link.href = url;
+                        link.setAttribute("download", "products.csv");
+                        document.body.appendChild(link);
+                        link.click();
+                        link.remove();
+                        toast.success("Export completed successfully!", {
+                            description: `${eanArray.length} items exported.`,
+                        });
+                        clearCart();
+                    } else {
+                        console.error("CSV export failed");
+                    }
+                }
+            },
+        })
+
+
     }
 
     return (
         <>
             <div>
                 <div className="flex justify-end items-center gap-2 py-2 pr-4">
-                    <Recycle className='cursor-pointer text-red-600' onClick={deleteLocalStorage}/>
+                    <Recycle className='cursor-pointer text-red-600' onClick={deleteLocalStorage} />
                     <Download
                         className="w-5 h-5 text-green-500 cursor-pointer transition-colors hover:text-green-700"
                         onClick={() => exportCSV(eanArray)}
@@ -67,7 +87,7 @@ export default function ProductClientList({ products }: ProductClientListProps) 
                     <span className="font-semibold text-gray-800">({eanArray.length})</span>
                 </div>
 
-                
+
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 justify-center">
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "./components/CartContext";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         </span>
         <CartProvider>
           {children}
+          <Toaster richColors  position="top-center" />
         </CartProvider>
       </body>
     </html>
