db67292a4977780be499e3716a9310a0318f76a4
Francis Vaz <francisxvaz@gmail.com>
2025-09-20 16:10:39 +1000

    top products api

---
+++ src/app/api/products/top/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import dbConnect from '@/app/lib/dbConnect';
+import Product from '@/app/models/Product';
+
+export async function GET(request: Request) {
+  await dbConnect();
+  try {
+    const randomProducts = await Product.aggregate([{ $sample: { size: 3 } }]);
+    return NextResponse.json({ products: randomProducts });
+  }
+  catch (ex) {
+
+  }
+}
